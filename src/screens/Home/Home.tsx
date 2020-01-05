81f8ea407999951d3f7df1f043c833d8441310a1
borithhor <hor.borith@realwat.net>
2020-01-05 14:21:39 +0700

    create header

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import {Container, Row, Col, Card, CardBody} from 'reactstrap';
+import { Container, } from 'reactstrap';
 
 const Home = () => {
   return (
     <Container>
-      <Row>
-        <Col>
-          <Card>
-            <CardBody>CardBody</CardBody>
-          </Card>
-        </Col>
-      </Row>
+
     </Container>
   );
 };
